121b93187837917f37febf1684de9a207d1600a4
klapalme <kl.jr@aol.com>
2017-02-19 13:06:18 -0500

    update local config processing

--- scripts/make-sb.sh
+++ scripts/make-sb.sh
@@ -7,19 +7,19 @@
 if [ ! -d sites/first ]; then
    # Check to see if the developer make file is available.
    if [ -f build/springboard-developer.make ]; then
-     drush make --no-gitinfofile build/springboard-developer.make $directory;
+     drush make --no-gitinfofile build/springboard-developer.make sites/first;
      # add springboard to drupal core's .gitignore.
      cd sites/first echo sites/all >> .gitignore; echo profiles/sbsetup >> .gitignore;
      cd ../../
    else
      # If no developer make, use the standard make file.
-     drush make --no-gitinfofile --working-copy build/springboard-mtsb.make $directory;
+     drush make --no-gitinfofile --working-copy build/springboard-mtsb.make sites/first;
    fi;
 fi;
 
 CONFIG_FILE=config/local.config.yml
 
-function parse_yaml {
+function parse_yaml_for_vhosts {
    local prefix=$2
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
    sed -ne "s|^\($s\):|\1|" \
@@ -31,24 +31,22 @@ function parse_yaml {
       for (i in vname) {if (i > indent) {delete vname[i]}}
       if (length($3) > 0) {
         if ($2 == "documentroot") {
-             dirname=substr($3,2)
-             sub(/\//, "__", dirname)
+             split($3, dirname, "/")
              vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-             printf("%s%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, "__"dirname, dirname);
+             printf("%s%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, "__"dirname[3], dirname[3]);
          }
       }
    }'
 }
 
 # Parse the local config yml file into the global vars.
-eval $(parse_yaml ${CONFIG_FILE})
-
+eval $(parse_yaml_for_vhosts ${CONFIG_FILE})
+#( set -o posix ; set ) | more
 for vhost in ${!apache_vhosts__documentroot*}
     do
         # Get the docroot directory name.
         directory=${!vhost}
         directory=${directory/__/\/}
-
         if [ ! -d $directory ]; then
 
            echo "Type the branch name that you want to check out into the directory $directory, followed by [ENTER]:"
@@ -59,13 +57,13 @@ for vhost in ${!apache_vhosts__documentroot*}
 
            # Check to see if the developer make file is available.
            if [ -f build/springboard-developer.make ]; then
-             drush make --no-gitinfofile build/springboard-developer.make $directory;
+             drush make --no-gitinfofile build/springboard-developer.make sites/$directory;
              # add springboard to drupal core's .gitignore.
              cd sites/$directory echo sites/all >> .gitignore; echo profiles/sbsetup >> .gitignore;
              cd ../../
            else
              # Use the standard make file.
-             drush make --no-gitinfofile --working-copy build/springboard-mtsb.make $directory;
+             drush make --no-gitinfofile --working-copy build/springboard-mtsb.make sites/$directory;
            fi;
            # Create a sustainer.key file in sites/default/files
            mkdir -p sites/$directory/sites/default/files
--- scripts/post-provision.sh
+++ scripts/post-provision.sh
@@ -6,3 +6,7 @@ cd ${PROJECT_ROOT}/sites/first
 set -x
 # Set Drupal variable to above directory 'encrypt_secure_key_path'
 drush vset encrypt_secure_key_path ${PROJECT_ROOT}/sites/first/sites/default/files/
+
+
+#cd ${PROJECT_ROOT}/sites/tickets
+#drush site-install sbsetup -y --site-name=tickets --account-name=admin  --account-pass=admin --db-url=mysql://drupal:drupal@localhost/tickets
