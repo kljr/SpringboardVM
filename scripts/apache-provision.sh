35898d00d9d8ff02980535eab30653b6dfa62074
klapalme <kl.jr@aol.com>
2017-02-25 12:59:33 -0500

    include all roles

--- scripts/apache-provision.sh
+++ scripts/apache-provision.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-DR_NEW_VHOSTS=TRUE DRUPALVM_ANSIBLE_ARGS='--start-at-task=*DruVMoser baby*' ANSIBLE_ROLES_PATH=vendor/geerlingguy/drupal-vm/provisioning/roles vagrant provision
+DR_NEW_VHOSTS=TRUE DRUPALVM_ANSIBLE_ARGS='--start-at-task=*DruVMoser baby*' vagrant provision
