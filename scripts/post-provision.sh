cc5be29bc7dbdb11c9d44e8318808bb720559d28
klapalme <kl.jr@aol.com>
2017-03-01 21:01:21 -0500

    remove typo

--- scripts/post-provision.sh
+++ scripts/post-provision.sh
@@ -49,7 +49,7 @@ if [ -f ${LOCAL_CONFIG_FILE} ]; then
 fi;
 
 cp /vagrant/templates/drushrc.php /home/vagrant/.drush/drushrc.php
-chown vagrant:vagrant ./home/vagrant/.drush/drushrc.php
+chown vagrant:vagrant /home/vagrant/.drush/drushrc.php
 
 if [ ! -f /home/vagrant/.bashrc_druv ]; then
   cat /vagrant/templates/bashrc_guest_include >> /home/vagrant/.bashrc
