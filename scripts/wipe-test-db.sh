328e0a8efa0baf46288f764063e7acdcaafbaaa8
klapalme <kl.jr@aol.com>
2018-03-15 14:07:27 -0400

    add a wipe-db alias

--- scripts/wipe-test-db.sh
+++ scripts/wipe-test-db.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-drush @sb-sb_testing sql-drop -y
-gunzip < sites/sb_testing/.circleci/springboard.sql.gz | drush @sb-sb_testing sql-cli
-drush @sb-sb_testing updb -y
-drush @sb-sb_testing dm-prep
+
+script_dir="$(dirname "$0")"
+source "$script_dir/parse-yaml.sh"
+cd $script_dir
+export PATH=$PWD:$PATH
+cd ../
+
+MAIN_CONFIG_FILE=config/config.yml
+eval $(parse_yaml ${MAIN_CONFIG_FILE})
+echo "Type the springboard root directory of the site who's db you wish to replace [ENTER]:"
+read dir
+sa=@${springboard_vm_drush_alias_uniqifier}${dir}
+drush  $sa sql-drop -y
+gunzip < sites/${dir}/.circleci/springboard.sql.gz | drush $sa sql-cli
+drush $sa updb -y
+drush $sa vset encrypt_secure_key_path ${dir}/web/sites/default/files/
+drush $sa cc all
+
