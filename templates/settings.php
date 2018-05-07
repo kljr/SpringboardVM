05e7a35cd0dae2930ed83982be6e5e74f9898353
klapalme <kl.jr@aol.com>
2018-05-07 09:05:35 -0400

    add host param to db config

--- templates/settings.php
+++ templates/settings.php
@@ -251,6 +251,7 @@ $databases = array(
       'database' => 'sb_default',
       'username' => 'root',
       'password' => 'root',
+      'host' => '127.0.0.1',
       'port' => '3306',
       'prefix' => '',
       'init_commands' => array(
