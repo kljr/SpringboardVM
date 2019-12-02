88cafbd6e7c3dc18af1fb56eb5ccb3f0bc27d4f6
klapalme <kl.jr@aol.com>
2019-12-02 08:16:13 -0500

    update to php 7.2, latest DrupalVM

--- scripts/vm-only/wipe-test-db.sh
+++ scripts/vm-only/wipe-test-db.sh
@@ -7,9 +7,16 @@ echo "Enter the project root directory of the site whose db you wish to replace 
 read dir
 sa=@${springboard_vm_drush_alias_uniqifier}${dir}
 drush  $sa sql-drop -y
-gunzip < ${SBVM_ROOT}/${dir}/.circleci/springboard.sql.gz | drush $sa sql-cli
-drush $sa updb -y
+ if [ -f ${SBVM_ROOT}/${dir}/.circleci/database.initdb.d/springboard.sql.gz ]; then
+  gunzip < ${SBVM_ROOT}/${dir}/.circleci/database.initdb.d/springboard.sql.gz | drush $sa sql-cli
+   else
+   gunzip < ${SBVM_ROOT}/${dir}/.circleci/springboard.sql.gz | drush $sa sql-cli
+ fi;drush $sa updb -y
+
 drush $sa upwd admin --password=admin -y
 drush $sa vset encrypt_secure_key_path ${SBVM_ROOT}/${dir}/web/sites/default/files/
 drush $sa cc all
 chmod 775 ${SBVM_ROOT}/${dir}/web/sites/default
+if [ -f  ${SBVM_ROOT}/${dir}/.circleci/drush-scripts/fix-encrypt-paths.php ]; then
+  drush $sa scr ${SBVM_ROOT}/${dir}/.circleci/drush-scripts/fix-encrypt-paths.php '../.circleci/private-files/openssl'
+fi;
